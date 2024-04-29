0621c18134a1624b956928d0313efb47dccf162c
Divyansh Pawar <112549562+Hshshshsh454@users.noreply.github.com>
2024-04-29 13:24:39 +0530

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553    
+let accountEmail = "divyanshpawar03"
+var acccontPassword = 12345 
+accountCity = "Jaipur"
+
+// accontId = 2 // not allowed 
+
+accountEmail = "hchc.com"
+accountpassword = 15154749
+accountCity = "Bengaluru"
+
+console.log(accontId);
+
+
+console.table([accountId,accountEmail,acccontPassword,accountCity])
+
